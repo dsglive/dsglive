deeb2302dfeae71efac506e7b7c15c0ef5cf8b8e
Code It Like Miley <codeitlikemiley@gmail.com>
2018-06-14 18:35:05 +0800

    added ShipperResource

---
+++ app/Http/Resources/User/ShipperResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\User;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class ShipperResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'        => $this->id,
+            'active'    => $this->active,
+            'name'      => $this->name ?? 'Unknown',
+            'email'     => $this->email,
+            'phone'     => $this->phone,
+            'address_1' => $this->address_1,
+            'address_2' => $this->address_2,
+            'city'      => $this->city,
+            'state'     => $this->state,
+            'zip'       => $this->zip,
+            'country'   => $this->country,
+            'notes'     => $this->notes
+        ];
+    }
+}
