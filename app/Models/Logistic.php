0a17778c510bbce34723094b8021ad1040ee4c87
Code It Like Miley <codeitlikemiley@gmail.com>
2018-06-27 11:56:31 +0800

    updated cast , guarded and added hasMany packages relationship

--- app/Models/Logistic.php
+++ app/Models/Logistic.php
@@ -12,15 +12,7 @@ class Logistic extends Model
     protected $casts = [
         'customer_id'    => 'integer',
         'client_id'      => 'integer',
-        'start_time'     => 'datetime:Y-m-d H:m',
-        'end_time'       => 'datetime:Y-m-d H:m',
-        'prep_time'      => 'float',
-        'travel_time'    => 'float',
-        'clean_up_time'  => 'float',
         'total_time'     => 'float',
-        'rate'           => 'float',
-        'surcharge'      => 'float',
-        'total_charges'  => 'float',
         'date_delivered' => 'date:Y-m-d',
         'packages'       => 'array'
     ];
@@ -34,7 +26,17 @@ class Logistic extends Model
     ];
 
     /**
+     * @var array
+     */
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    /**
      * @var string
      */
     protected $table = 'logistics';
+
+    public function packages()
+    {
+        return $this->hasMany(Package::class);
+    }
 }
