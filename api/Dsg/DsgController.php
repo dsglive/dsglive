55355a5882f44d1166d4ded5c89f981b9fa2a10e
Code It Like Miley <codeitlikemiley@gmail.com>
2018-08-02 23:45:53 +0800

    make query ASC at create_at

--- api/Dsg/DsgController.php
+++ api/Dsg/DsgController.php
@@ -163,7 +163,7 @@ class DsgController extends Controller
      */
     public function index(Request $request)
     {
-        $dsg = Dsg::active()->get();
+        $dsg = Dsg::active()->orderBy('created_at', 'ASC')->get();
         return DsgResource::collection($dsg); // remove pagination
     }
 
@@ -172,7 +172,7 @@ class DsgController extends Controller
      */
     public function indexArchived(Request $request)
     {
-        $dsg = Dsg::onlyTrashed()->active()->get();
+        $dsg = Dsg::onlyTrashed()->active()->orderBy('created_at', 'ASC')->get();
         return DsgResource::collection($dsg); // remove pagination
     }
 
@@ -307,7 +307,7 @@ class DsgController extends Controller
      */
     public function warehouse(Request $request)
     {
-        $dsg = Dsg::warehouse()->get();
+        $dsg = Dsg::warehouse()->orderBy('created_at', 'ASC')->get();
         return DsgResource::collection($dsg); // remove pagination
     }
 
