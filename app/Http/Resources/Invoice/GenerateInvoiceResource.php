090c2d7d14072725c6f4b6798224fbe3704e945e
Uriah Galang <codeitlikemiley@gmail.com>
2018-09-02 16:48:03 +0800

    format comment

--- app/Http/Resources/Invoice/GenerateInvoiceResource.php
+++ app/Http/Resources/Invoice/GenerateInvoiceResource.php
@@ -65,23 +65,23 @@ class GenerateInvoiceResource extends Resource
                     $date_started = Carbon::parse($request->input('date_started'));
                     $days         = 0;
 
-//! [case1] if item is not yet delivered we will use date_ended as date_ended if it is delivered we will use that as our date_ended
+                    //! [case1] if item is not yet delivered we will use date_ended as date_ended if it is delivered we will use that as our date_ended
 
                     if (!$item->date_delivered) {
                         $date_ended = $request->input('date_ended');
 
-//! [case1]  if no existing last date invoice we will get the difference between the date_started and date_ended
+                        //! [case1]  if no existing last date invoice we will get the difference between the date_started and date_ended
 
                         if (!$item->last_invoice_at) {
                             $days = $item->date_delivered->diffInDays($item->date_received);
 
-//! [case1]  also if the date_received < date_started we will  add that days to the difference of date_started and date_ended
+                            //! [case1]  also if the date_received < date_started we will  add that days to the difference of date_started and date_ended
 
                             if ($item->date_received < $date_started) {
                                 $days += $item->date_received->diffInDays($date_started);
                             }
 
-                            //! [case1]  if there is an existing last date invoice we will get the difference of last date invoice and date ended.
+                        //! [case1]  if there is an existing last date invoice we will get the difference of last date invoice and date ended.
                         } else {
                             $date_ended = Carbon::parse($request->input('date_ended'));
                             $days       = $item->last_invoice_at->difference($date_ended);
@@ -89,17 +89,17 @@ class GenerateInvoiceResource extends Resource
 
                         //! [case2] if item is delivered we will use that as our date_ended
                     } else {
-// this is the date_ended by default
+                        // this is the date_ended by default
 
-//$date_ended = $item->date_delivered;
+                        //$date_ended = $item->date_delivered;
 
-//! [case2] if there is no last invoice yet
+                        //! [case2] if there is no last invoice yet
 
-//! we will get difference between date_delivered and date_received
+                        //! we will get difference between date_delivered and date_received
                         if (!$item->last_invoice_at) {
                             $days = $item->date_delivered->diffInDays($item->date_received);
 
-//! [case2]  also if the date_received < date_started we will  add that days to the difference of date_started and date_ended
+                            //! [case2]  also if the date_received < date_started we will  add that days to the difference of date_started and date_ended
 
                             if ($item->date_received < $date_started) {
                                 $days += $item->date_received->diffInDays($date_started);
